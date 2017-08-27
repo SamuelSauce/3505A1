215724e688c71a0420fb8d088925f337a6747d69
SamuelSauce <samuelfbridge@gmal.com>
2017-08-27 16:44:53 -0600

    updated for input

--- EcoSim.cpp
+++ EcoSim.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include <cstdio>
 
 
 void updatePopulations(double g, double p, double c, double m, double K,
                        double& numRabbits, double& numFoxes);
 
 int main(){
-	
+	double numRabbits;
+	double numFoxes;
+
+	cout << "Enter initial number of rabbits, followed by initial number of foxes." << endl;
+	cin >> numRabbits >> numFoxes >> endl;
+
+	cout << "You entered" << numRabbits << numFoxes << endl;
 }
